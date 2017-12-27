81757fc891faefb0b2a693e2ccdd50780baf7c1e
ZiyuJin <jinziyu2016@hotmail.com>
2017-12-28 02:15:47 +0800

    delete these files, since they are moved to "sharehosting folder"

--- htdocs/wp-content/plugins/learnpress-import-export/compatible/v0.9.x.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tu
- * Date: 1/18/2016
- * Time: 9:56 AM
- */
--- htdocs/wp-content/themes/buddydocs/searchform.php
+++
@@ -1,4 +0,0 @@
-<form role="search" method="get" id="searchform" action="<?php echo esc_url( site_url( '/' ) ); ?>" >
-    <input type="text" value="<?php  echo get_search_query();  ?>"  placeholder="<?php _e( 'Search Keyword','buddy-docs' );?>" name="s" id="s" class="search-terms"/>
-    <input type="submit" id="searchsubmit" value=" " />
-</form>
